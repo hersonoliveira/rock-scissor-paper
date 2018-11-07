9a7bfaf86493ec9843c4c126d816d45ed1083829
Herson Oliveira <oliveira.herson@gmail.com>
2018-11-07 22:41:54 +0100

    Include test class

--- src/test/java/HandTest.java
+++ src/test/java/HandTest.java
@@ -1,2 +1,21 @@
+import com.tyntec.game.model.Hand;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 public class HandTest {
+
+    private Hand hand;
+
+    @Test
+    public void winAgainstTest() {
+        hand = Hand.PAPER;
+        assertTrue(hand.winAgainst(Hand.ROCK));
+
+        hand = Hand.SCISSORS;
+        assertTrue(hand.winAgainst(Hand.PAPER));
+
+        hand = Hand.ROCK;
+        assertTrue(hand.winAgainst(Hand.SCISSORS));
+    }
 }
